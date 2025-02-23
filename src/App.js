dcb77b6bf7f7e8f2aa38ce3eaa8c271af91398ab
Jose Fabio <J053Fabi0@users.noreply.github.com>
2025-02-23 17:17:54 -0600

    Progress Bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import ProgressBar from "./ProgressBar";
 
-function App() {
+export default function App() {
+  const [progress, setProgress] = useState(0);
+
+  const handleIncrease = () => {
+    setProgress((p) => {
+      if (p >= 100) return 0;
+      return p + 10;
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ProgressBar progress={progress} />
+      <button onClick={handleIncrease}>{progress >= 100 ? "Reset" : "Increase"}</button>
     </div>
   );
 }
-
-export default App;
